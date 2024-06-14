a6b4102d0957ce95de1d4bd44c461e9d1423bb5d
fanchengyan <fanchy14@lzu.edu.cn>
2024-06-14 10:26:26 +0800

    refine example

--- redcapp_example_era5.py
+++ redcapp_example_era5.py
@@ -1,33 +1,50 @@
-from redcapp import data_manager as dm
-from redcapp import redcappTemp, raster2nc
 from pathlib import Path
 from time import time
+
 import numpy as np
 
-dem_file = '/media/fanchy/Seagate/LicSAR_QTP/descending/106D_05049_131313/temperature/era5/result/DEM_cliped.tif'
+from redcapp import data_manager as dm
+from redcapp import raster2nc, redcappTemp
+
+# set the directory containing all raw data and output data
+dir_data = Path("/Volumes/Data/GeoData/YNG/Temperature")
+
+folder_out = dir_data / "result"
+if not folder_out.is_dir():
+    folder_out.mkdir(parents=True)
 
 
-folder = Path(
-    '/media/fanchy/Seagate/LicSAR_QTP/descending/106D_05049_131313/temperature/era5')
-dem_nc = '/media/fanchy/Seagate/LicSAR_QTP/descending/106D_05049_131313/temperature/era5/result/DEM_cliped.nc'
+dem_file = dir_data / "your_dem.tif"
 
-spatTopo_out = folder / 'result/spatTopo.nc'
-spatTemp_out = folder / 'result//spatialT.nc'
+# output dem file in netcdf format
+dem_nc = dir_data / "DEM_WGS84.nc"
+
+spatTopo_out = folder_out / "result/spatTopo.nc"
+spatTemp_out = folder_out / "result//spatialT.nc"
 
 
 ############# For first time only: install CDS_API_key  ##################
-# dm.install_CDS_API_key(url='https://cds.climate.copernicus.eu/api/v2',
-#                        key='your key')
 
+### get your key from https://cds.climate.copernicus.eu/api-how-to
+### and replace 'your key' with your key and uncomment the following lines:
+
+# dm.install_CDS_API_key(
+#     url="https://cds.climate.copernicus.eu/api/v2",
+#     key="your key",
+# )
+
+############# For first time only: install CDS_API_key  ##################
+
+# convert DEM to netcdf format in WGS84
 raster2nc(dem_file, dem_nc)
 
 
-era5 = dm.ERA5_Manager(folder)
+era5 = dm.ERA5_Manager(folder_out)
 
 pl = era5.get_pressure_levels(min=500, max=1000)
-year, month, day, t = era5.generate_datetime('2014', '2021', freq='1D')
-area = era5.get_area_from_DEM(dem_file)
-date = era5.get_date_range('2014-01-01', '2021-12-31')
+year, month, day, t = era5.generate_datetime("2017", "2023", freq="1D")
+area = era5.get_area_from_DEM(dem_nc)
+date = era5.get_date_range("2017-01-01", "2023-5-1")
 
 ################  download data  ########################
 era5.retrieve_single_levels(year, month, day, t, area)
@@ -35,19 +52,9 @@ era5.retrieve_pressure_levels(pl, year, month, day, t, area)
 
 
 ############### merge and format data ###################
-pl = era5.merge_nc(
-    name='pl',
-    merged_file=None,
-    day_mean=True,
-    format=True
-)
+pl = era5.merge_nc(name="pl", merged_file=None, day_mean=True, format=True)
 
-sa = era5.merge_nc(
-    name='sl',
-    merged_file=None,
-    day_mean=True,
-    format=True
-)
+sa = era5.merge_nc(name="sl", merged_file=None, day_mean=True, format=True)
 
 geop = era5.format_nc(era5.geop)
 
@@ -64,8 +71,10 @@ Redcapp = redcappTemp(geop, sa, pl, date, dem_nc)
 Redcapp.extractSpatialDataNCF_TS(spatTopo_out, spatTemp_out)
 
 
-elapsed_time = time()-time_s
-hour = int(elapsed_time/3600)
-minite = int(np.mod((elapsed_time/60), 60))
+elapsed_time = time() - time_s
+hour = int(elapsed_time / 3600)
+minite = int(np.mod((elapsed_time / 60), 60))
 sec = int(np.mod(elapsed_time, 60))
+print("\nElapsed time: {0:02}h {1:02}m {2:02}s".format(hour, minite, sec))
+print("\nElapsed time: {0:02}h {1:02}m {2:02}s".format(hour, minite, sec))
 print("\nElapsed time: {0:02}h {1:02}m {2:02}s".format(hour, minite, sec))
